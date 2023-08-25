3705d33a1eaae326943ec0de05a654028625fa8e
turtle <qq769711153@hotmail.com>
2023-08-25 20:11:01 +0800

    doc the Downloader completely

--- src/downloader.cpp
+++ src/downloader.cpp
@@ -139,17 +139,9 @@ int spawn_multi_downloads_and_join(
     return OK;
 }
 
-/**
- * @brief 下载器运行函数
- *
- * 该函数是下载器的主要运行函数。它通过初始化ACE库，建立与服务器的连接并登录，然后调用
- * spawn_multi_downloads_and_join 函数进行多线程下载和合并操作，最后关闭ACE库。
- *
- * @return 如果运行成功，则返回0；否则返回非零值。
- */
 int Downloader::run()
 {
-    // 处理控制连接void
+    // 发出多个下载线程，并等待线程完成
     int ret = spawn_multi_downloads_and_join(
             sock_, filepath_, threads_, savepath_.c_str(), sock_creator_);
     if (ret) {
--- src/include/downloader.h
+++ src/include/downloader.h
@@ -18,15 +18,27 @@
 
 using std::string;
 
+/**
+ * @brief Downloader 类用于下载文件
+ */
 class Downloader
 {
-    string filepath_;
-    int threads_;
-    string savepath_;
-    SOCK sock_;
-    Sock_Creator sock_creator_;
+    string filepath_;           /**< 文件路径 */
+    int threads_;               /**< 线程数 */
+    string savepath_;           /**< 保存路径 */
+    SOCK sock_;                 /**< socket 信息 */
+    Sock_Creator sock_creator_; /**< socket 创建器 */
 
 public:
+    /**
+     * @brief Downloader 类的构造函数
+     *
+     * @param path 文件路径
+     * @param threads 线程数
+     * @param savepath 保存路径
+     * @param sock socket 信息
+     * @param sock_creator socket 创建器
+     */
     Downloader(
             string path,
             int threads,
@@ -40,5 +52,15 @@ public:
           sock_creator_(sock_creator)
     {
     }
+
+    /**
+     * @brief 下载器运行函数
+     *
+     * 该函数是下载器的主要运行函数。它通过初始化ACE库，建立与服务器的连接并登录，然后调用
+     * spawn_multi_downloads_and_join
+     * 函数进行多线程下载和合并操作，最后关闭ACE库。
+     *
+     * @return 如果运行成功，则返回0；否则返回非零值。
+     */
     int run();
 };
