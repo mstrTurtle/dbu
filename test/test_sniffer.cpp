f97f58a7bbb2375135e378e9f4a78dca9f074b7e
turtle <qq769711153@hotmail.com>
2023-08-25 15:05:26 +0800

    add sniffer test

---
+++ test/test_sniffer.cpp
@@ -0,0 +1,53 @@
+// test_sniffer.cpp
+#include "sniffer.h"
+#include <gtest/gtest.h>
+
+/**
+ * @brief 检验找得到的路径
+ *
+ */
+TEST(DbuSnifferTest, ValidPath)
+{
+    ACE_INET_Addr addr(21, "ftp.scutech.com");
+    Sock_Creator sock_creator =
+            make_logined_sock_creator(addr, "scutech", "dingjia");
+
+    Sniff_Hint hint{
+            .branch = "hotfix",
+            .subbranch = "56006-hana",
+            .option = "debug",
+            .arch = "x86_64",
+            .product = "gaussdb"};
+
+    // 连接到端点
+    SOCK sock;
+    sock_creator(sock);
+
+    // run sniffer，查找最新版本的product
+
+    Sniffer sniffer(addr, sock, hint);
+
+    string path;
+    int ret = sniffer.run(path);
+
+    ASSERT_EQ(ret, 0);
+    ASSERT_EQ(
+            path,
+            "/ftp_product_installer/dbackup3/rpm/hotfix/56006-hana/8.0.35618/"
+            "dbackup3-agent-gaussdb-8.0.35618-1.6586071.dbg.x86_64.rpm");
+}
+
+/**
+ * @brief 检验找不到的路径
+ *
+ */
+TEST(DbuSnifferTest, InvalidPath)
+{
+}
+
+int
+main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
